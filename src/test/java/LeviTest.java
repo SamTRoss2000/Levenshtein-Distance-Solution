845a6c3a12233ae0f31ce70540055b7c96d543bf
sross <sross@deloitte.co.uk>
2023-04-18 14:19:36 +0100

    Renamed the constant 'kittenString' to 'KITTEN_STRING' to better represent Java conventions'

--- src/test/java/LeviTest.java
+++ src/test/java/LeviTest.java
@@ -4,11 +4,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LeviTest {
-
+    private final static String KITTEN_STRING = "kitten";
     @Test
     void nullWordsAreIllegalArguments() {
         IllegalArgumentException nullLeviThrown = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Levi.distance(null, "kitten");
+            Levi.distance(null, KITTEN_STRING);
         });
         assertEquals("The distance method cannot accept null", nullLeviThrown.getMessage());
     }
@@ -16,7 +16,7 @@ public class LeviTest {
     @Test
     void nullSecondWordsAreIllegalArgumentsForDistance() {
         IllegalArgumentException nullDistanceThrown = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Levi.distance("kitten", null);
+            Levi.distance(KITTEN_STRING, null);
         });
 
         assertEquals("The distance method cannot accept null", nullDistanceThrown.getMessage());
@@ -24,8 +24,8 @@ public class LeviTest {
 
     @Test
     void emptyDistanceDefersToInputLength() {
-        String kittenString = "kitten";
-        assertEquals(kittenString.length(), Levi.distance("kitten", ""));
+        String kittenString = KITTEN_STRING;
+        assertEquals(kittenString.length(), Levi.distance(KITTEN_STRING, ""));
     }
 
     @Test
@@ -35,47 +35,42 @@ public class LeviTest {
 
     @Test
     void theDistanceMethodIgnoresBlankSpaces() {
-        assertEquals(0, Levi.distance("kitten", "kit ten"));
-        assertEquals(0, Levi.distance("kit ten", "kitten"));
+        assertEquals(0, Levi.distance(KITTEN_STRING, "kit ten"));
+        assertEquals(0, Levi.distance("kit ten", KITTEN_STRING));
     }
 
     @Test
     void theDistanceMethodIgnoresCapitalLetters() {
-        assertEquals(0, Levi.distance("kitten", "Kitten"));
-        assertEquals(0, Levi.distance("Kitten", "kitten"));
+        assertEquals(0, Levi.distance(KITTEN_STRING, "Kitten"));
+        assertEquals(0, Levi.distance("Kitten", KITTEN_STRING));
     }
 
     @Test
     void identicalStringsHaveNoDistance() {
-        assertEquals(0, Levi.distance("kitten", "kitten"));
+        assertEquals(0, Levi.distance(KITTEN_STRING, KITTEN_STRING));
     }
 
     @Test
     void distanceIncreasesBasedOnInputLengthDifference() {
-        assertEquals(1, Levi.distance("kitten", "kittens"));
-        assertEquals(1, Levi.distance("kitten", "kitte"));
+        assertEquals(1, Levi.distance(KITTEN_STRING, "kittens"));
+        assertEquals(1, Levi.distance(KITTEN_STRING, "kitte"));
     }
 
     @Test
     void distanceIncreasesBasedOnCharacterDifference() {
-        assertEquals(1, Levi.distance("kitten", "kipten"));
-        assertEquals(1, Levi.distance("kitten", "mitten"));
+        assertEquals(1, Levi.distance(KITTEN_STRING, "kipten"));
+        assertEquals(1, Levi.distance(KITTEN_STRING, "mitten"));
         String adjacentCharacterDifference = "kanten";
         String nonAdjacentCharacterDifference = "kattin";
-        assertEquals(2, Levi.distance("kitten", adjacentCharacterDifference));
-        assertEquals(2, Levi.distance("kitten", nonAdjacentCharacterDifference));
+        assertEquals(2, Levi.distance(KITTEN_STRING, adjacentCharacterDifference));
+        assertEquals(2, Levi.distance(KITTEN_STRING, nonAdjacentCharacterDifference));
     }
 
     // Does the distance() method return an accurate value when there are differing adjacent letters
     // and the words are of different lengths
     @Test
     void differenceReasonsCanBeCombined() {
-        assertEquals(3, Levi.distance("kitten", "kantenn"));
-        assertEquals(3, Levi.distance("kitten", "sitting"));
-    }
-
-    @Test
-    void LeviNoInternalState() {
-        assertEquals(1, Levi.distance("kitten", "kipten"));
+        assertEquals(3, Levi.distance(KITTEN_STRING, "kantenn"));
+        assertEquals(3, Levi.distance(KITTEN_STRING, "sitting"));
     }
 }
